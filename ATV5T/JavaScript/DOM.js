29c81e890a2e20d148518e0e167defad4852f4b9
Eduardo <eduardooffside@gmail.com>
2021-02-15 16:02:32 -0300

    js-dom

---
+++ ATV5T/JavaScript/DOM.js
@@ -0,0 +1,11 @@
+let div_p = document.getElementById("div-principal")
+console.log(div_p)
+
+let divs = document.getElementsByTagName("div")
+console.log(divs)
+
+//recupera pela classe
+//let div_class = document.getElementsByClassName("divs-divs")
+
+let div_dentro = document.querySelector(".div-principal .interior")
+console.log(div_dentro)
