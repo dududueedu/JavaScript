dde19a355f9694d356e53014feae066c54cddb26
Eduardo <eduardooffside@gmail.com>
2021-02-16 16:09:09 -0300

    js

---
+++ ATV5T/JavaScript/MenuHamburg/conduct.js
@@ -0,0 +1,9 @@
+let botao = document.querySelector(" .navegador .nav-cabecalho .meu-botao")
+console.log(botao)
+
+let menu = document.querySelector(".navegador .menu")
+console.log(menu)
+
+botao.addEventListener("click", function(){
+    menu.classList.toggle("active")
+})
