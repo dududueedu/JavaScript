59e63c35740a65402eb175034e23edba1a17a834
Eduardo <eduardooffside@gmail.com>
2021-02-22 13:31:51 -0300

    modal

--- ATV5T/JavaScript/Modal/javas.js
+++ ATV5T/JavaScript/Modal/javas.js
@@ -6,3 +6,17 @@ let overlayModal = document.querySelector(".overlay_modal")
 openModal.addEventListener("click", function(){
     overlayModal.classList.add("active")
 })
+
+let botao_close = document.querySelector(".overlay_modal .container_modal .footer .botao_close")
+botao_close.addEventListener("click", function(){
+    overlayModal.classList.remove("active")
+})
+
+let botaoX = document.querySelector(".overlay_modal .container_modal .header_modal .botaoX")
+botaoX.addEventListener("click", function(){
+    overlayModal.classList.remove("active")
+})
+
+window.addEventListener("click", function(event){
+    if(event.target == overlayModal) overlayModal.classList.remove("active")
+})
