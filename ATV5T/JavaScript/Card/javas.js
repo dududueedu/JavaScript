6513f012d518c9c868cf4e5a01f019e72da08109
Eduardo <eduardooffside@gmail.com>
2021-02-23 16:02:30 -0300

    cards

---
+++ ATV5T/JavaScript/Card/javas.js
@@ -0,0 +1,8 @@
+let form_nome = document.querySelector(".form_nome")
+let form_msg = document.querySelector(".form_msg")
+let botao_sumit = document.querySelector(".botao_submit") 
+
+botao_sumit.addEventListener("click", function(event){
+    event.preventDefault()
+    console.log("oiin")
+})
